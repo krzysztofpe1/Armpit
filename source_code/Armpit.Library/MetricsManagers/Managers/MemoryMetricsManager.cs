b84e27b3039e450f98aef52af05c6b1deff566e6
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2024-06-29 23:20:28 +0200

    Added SystemResourceMetrics and MemoryMetricsManager.

---
+++ source_code/Armpit.Library/MetricsManagers/Managers/MemoryMetricsManager.cs
@@ -0,0 +1,68 @@
+﻿using Armpit.Library.DataModels.Metrics;
+using System.Diagnostics;
+
+namespace Armpit.Library.MetricsManagers.Managers;
+
+public class MemoryMetricsManager : MetricsManagerPlatformDivisionBase<MemoryMetrics>
+{
+    protected override async Task<MemoryMetrics> GetUnixMetricsAsync()
+    {
+        var output = "";
+
+        var info = new ProcessStartInfo
+        {
+            FileName = "/bin/bash",
+            Arguments = "-c \"free -m\"",
+            RedirectStandardOutput = true
+        };
+
+        using (var process = Process.Start(info))
+        {
+            output = await process.StandardOutput.ReadToEndAsync();
+            await process.WaitForExitAsync();
+        }
+
+        var lines = output.Split("\n");
+        var memory = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        var metrics = new MemoryMetrics
+        {
+            Total = double.Parse(memory[1]),
+            Used = double.Parse(memory[2]),
+            Free = double.Parse(memory[3])
+        };
+
+        return metrics;
+    }
+
+    protected override async Task<MemoryMetrics> GetWindowsMetricsAsync()
+    {
+        var output = "";
+
+        var info = new ProcessStartInfo
+        {
+            FileName = "wmic",
+            Arguments = "OS get FreePhysicalMemory,TotalVisibleMemorySize /Value",
+            RedirectStandardOutput = true
+        };
+
+        using (var process = Process.Start(info))
+        {
+            output = await process.StandardOutput.ReadToEndAsync();
+            await process.WaitForExitAsync();
+        }
+
+        var lines = output.Trim().Split("\n");
+        var freeMemoryParts = lines[0].Split("=", StringSplitOptions.RemoveEmptyEntries);
+        var totalMemoryParts = lines[1].Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+        var metrics = new MemoryMetrics
+        {
+            Total = Math.Round(double.Parse(totalMemoryParts[1]) / 1024, 0),
+            Free = Math.Round(double.Parse(freeMemoryParts[1]) / 1024, 0)
+        };
+        metrics.Used = metrics.Total - metrics.Free;
+
+        return metrics;
+    }
+}
---
+++ source_code/Armpit.Library/SystemResourceMonitor.cs
@@ -0,0 +1,33 @@
+﻿using Armpit.Library.DataModels.Metrics;
+using Armpit.Library.MetricsManagers;
+using Armpit.Library.MetricsManagers.Managers;
+using System.Diagnostics;
+
+namespace Armpit.Library;
+
+public class SystemResourceMonitor
+{
+
+    #region Private vars
+
+    private IMetricsManager<MemoryMetrics> _memoryManager;
+    private IMetricsManager<CpuMetrics> _cpuManager;
+
+    #endregion
+    public SystemResourceMonitor()
+    {
+        _memoryManager = new MemoryMetricsManager();
+    }
+
+    #region Public Methods
+
+    public float GetCurrentCpuUsage()
+    {
+        return 0;
+    }
+
+
+
+    #endregion
+
+}
